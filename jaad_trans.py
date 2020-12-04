89bdc10900c6f3eb22e3d65ea36e8808aa0d0798
Dongxu <74026343+DongxuGuo1997@users.noreply.github.com>
2020-12-04 22:52:03 +0100

    Add files via upload

---
+++ jaad_trans.py
@@ -0,0 +1,262 @@
+import os
+import numpy as np
+import pickle
+import copy
+
+
+# --------------------------------------------------------------------
+def get_split_vids(split_vids_path, image_set, subset='default'):
+    """
+        Returns a list of video ids for a given data split
+        :param image_set: Data split, train, test, val
+        :return: The list of video ids
+        """
+    assert image_set in ["train", "test", "val", "all"]
+    vid_ids = []
+    sets = [image_set] if image_set != 'all' else ['train', 'test', 'val']
+    for s in sets:
+        vid_id_file = os.path.join(split_vids_path, subset, s + '.txt')
+        with open(vid_id_file, 'rt') as fid:
+            vid_ids.extend([x.strip() for x in fid.readlines()])
+
+    return vid_ids
+
+
+def get_pedb_ids_jaad(annotations, vid):
+    """
+    Get pedestrians'(with behavior tags) ids in specific video.
+    :param: dataset: JAAD raw data in dictionary form
+            vid : video id (str)
+    :return: pedestrians' ids
+
+    """
+    pedb_ids = []
+    ped_keys = list(annotations[vid]['ped_annotations'].keys())
+    for key in ped_keys:
+        if 'b' in key:
+            pedb_ids.append(key)
+
+    return pedb_ids
+
+
+def get_pedb_info_jaad(annotations, vid):
+    # get pedb information,i.e. frames,bbox,occlusion, actions(walking or not) and other attributes
+    ids = get_pedb_ids_jaad(annotations, vid)
+    dataset = annotations
+    pedb_info = {}
+    for idx in ids:
+        pedb_info[idx] = {}
+        pedb_info[idx]['frames'] = []
+        pedb_info[idx]['bbox'] = []
+        pedb_info[idx]['occlusion'] = []
+        pedb_info[idx]['action'] = []
+        pedb_info[idx]['cross'] = []
+        pedb_info[idx]['looking'] = []
+        pedb_info[idx]['gesture'] = []
+        pedb_info[idx]['attributes'] = []
+
+        frames = copy.deepcopy(dataset[vid]['ped_annotations'][idx]['frames'])
+        bbox = copy.deepcopy(dataset[vid]['ped_annotations'][idx]['bbox'])
+        occlusion = copy.deepcopy(dataset[vid]['ped_annotations'][idx]['occlusion'])
+        action = copy.deepcopy(dataset[vid]['ped_annotations'][idx]['behavior']['action'])
+        looking = copy.deepcopy(dataset[vid]['ped_annotations'][idx]['behavior']['look'])
+        cross = copy.deepcopy(dataset[vid]['ped_annotations'][idx]['behavior']['cross'])
+        hand_gesture = copy.deepcopy(dataset[vid]['ped_annotations'][idx]['behavior']['hand_gesture'])
+        nod = copy.deepcopy(dataset[vid]['ped_annotations'][idx]['behavior']['nod'])
+        attributes = copy.deepcopy(dataset[vid]['ped_annotations'][idx]["attributes"])
+
+        for i in range(len(frames)):
+            if action[i] in [0, 1]:  # sanity check if behavior label exists
+                pedb_info[idx]['action'].append(action[i])
+                pedb_info[idx]['frames'].append(frames[i])
+                pedb_info[idx]['bbox'].append(bbox[i])
+                pedb_info[idx]['occlusion'].append(occlusion[i])
+                pedb_info[idx]['cross'].append(cross[i])
+                pedb_info[idx]['looking'].append(looking[i])
+                if nod[i] == 1:
+                    pedb_info[idx]["gesture"].append(4)
+                else:
+                    if hand_gesture[i] == 4:
+                        pedb_info[idx]["gesture"].append(5)
+                    else:
+                        pedb_info[idx]["gesture"].append(hand_gesture[i])
+
+        pedb_info[idx]["attributes"] = attributes
+
+    return pedb_info
+
+
+def filter_None(x):
+    # small help function to filter None in list
+    if x is None:
+        return False
+    else:
+        return True
+
+
+def pedb_info_clean_jaad(annotations, vid):
+    """
+     Remove all frames has occlusion tag = 2 (fully occluded)
+    """
+    pedb_info = get_pedb_info_jaad(annotations, vid)
+    pids = list(pedb_info.keys())
+    # remove all frames with occlusion tag=2
+    for idx in pids:
+        occ = np.array(pedb_info[idx]['occlusion'])
+        full_occ = np.flatnonzero(occ == 2)
+        # set fully occluded frames to None
+        for i in range(len(full_occ)):
+            pedb_info[idx]['frames'][full_occ[i]] = None
+            pedb_info[idx]['bbox'][full_occ[i]] = None
+            pedb_info[idx]['action'][full_occ[i]] = None
+            pedb_info[idx]['occlusion'][full_occ[i]] = None
+            pedb_info[idx]['cross'][full_occ[i]] = None
+            pedb_info[idx]['looking'][full_occ[i]] = None
+            pedb_info[idx]['gesture'][full_occ[i]] = None
+
+        # filter all None values
+        pedb_info[idx]['frames'] = list(filter(filter_None, pedb_info[idx]['frames']))
+        pedb_info[idx]['bbox'] = list(filter(filter_None, pedb_info[idx]['bbox']))
+        pedb_info[idx]['action'] = list(filter(filter_None, pedb_info[idx]['action']))
+        pedb_info[idx]['occlusion'] = list(filter(filter_None, pedb_info[idx]['occlusion']))
+        pedb_info[idx]['cross'] = list(filter(filter_None, pedb_info[idx]['cross']))
+        pedb_info[idx]['looking'] = list(filter(filter_None, pedb_info[idx]['looking']))
+        pedb_info[idx]['gesture'] = list(filter(filter_None, pedb_info[idx]['gesture']))
+
+    return pedb_info
+
+
+def add_transition_labels_jaad(dataset, verbose=False):
+    """
+    Add stop & go transition labels for every frame
+    """
+    all_wts = 0  # walking to standing(Stop)
+    all_stw = 0  # standing to walking (Go)
+    pids = list(dataset.keys())
+    for idx in pids:
+        action = dataset[idx]['action']
+        frames = dataset[idx]['frames']
+        n_frames = len(frames)
+        dataset[idx]['next_transition'] = []
+        stw_time = []
+        wts_time = []
+
+        for j in range(len(action) - 1):
+            # stop and go transition
+            if action[j] == 0 and action[j + 1] == 1:
+                all_stw += 1
+                stw_time.append(frames[j + 1])
+            elif action[j] == 1 and action[j + 1] == 0:
+                all_wts += 1
+                wts_time.append(frames[j + 1])
+
+        # merge
+        trans_time = np.array(sorted(stw_time + wts_time))
+        # set transition tag
+        for i in range(n_frames):
+            t_frame = frames[i]
+            future_trans = trans_time[trans_time >= t_frame]
+            if future_trans.size > 0:
+                next_trans = future_trans[0]
+                dataset[idx]['next_transition'].append(next_trans - t_frame)
+            else:
+                dataset[idx]['next_transition'].append(None)
+
+    if verbose:
+        print('\n')
+        print('----------------------------------------------------------------')
+        print(f'Total number of standing to walking transitions(raw): {all_stw}')
+        print(f'Total number of walking to standing transitions(raw): {all_wts}')
+
+    return None
+
+
+def build_pedb_dataset_jaad(jaad_anns_path, split_vids_path, image_set="all", verbose=False):
+    """
+    Build pedestrian dataset from jaad annotations
+    """
+    jaad_anns = pickle.load(open(jaad_anns_path, 'rb'))
+    pedb_dataset = {}
+    vids = get_split_vids(split_vids_path, image_set, subset='default')
+    for vid in vids:
+        pedb_info = pedb_info_clean_jaad(jaad_anns, vid)
+        pids = list(pedb_info.keys())
+        for idx in pids:
+            pedb_dataset[idx] = {}
+            pedb_dataset[idx]['video_number'] = vid
+            pedb_dataset[idx]['frames'] = pedb_info[idx]['frames']
+            pedb_dataset[idx]['bbox'] = pedb_info[idx]['bbox']
+            pedb_dataset[idx]['action'] = pedb_info[idx]['action']
+            pedb_dataset[idx]['occlusion'] = pedb_info[idx]['occlusion']
+            pedb_dataset[idx]["cross"] = pedb_info[idx]["cross"]
+            pedb_dataset[idx]["gesture"] = pedb_info[idx]["gesture"]
+            pedb_dataset[idx]["looking"] = pedb_info[idx]["looking"]
+            pedb_dataset[idx]["attributes"] = pedb_info[idx]["attributes"]
+
+    add_transition_labels_jaad(pedb_dataset, verbose)
+
+    return pedb_dataset
+
+
+class jaad_trans_dataset():
+    """
+     dataset class for transition-related pedestrian samples in JAAD
+    """
+
+    def __init__(self, jaad_anns_path, split_vids_path, image_set="all", verbose=False):
+        assert image_set in ['train', 'test', 'val', "all"], " Name should be train, test, val or all"
+        self.dataset = build_pedb_dataset_jaad(jaad_anns_path, split_vids_path, image_set, verbose)
+        self.name = image_set
+
+    def extract_trans_frame(self, mode="GO", verbose=False):
+
+        dataset = self.dataset
+        assert mode in ["GO", "STOP"], "Transition type should be STOP or GO"
+        ids = list(dataset.keys())
+        samples = {}
+        j = 0
+        for idx in ids:
+            vid_id = copy.deepcopy(dataset[idx]['video_number'])
+            frames = copy.deepcopy(dataset[idx]['frames'])
+            bbox = copy.deepcopy(dataset[idx]['bbox'])
+            action = copy.deepcopy(dataset[idx]['action'])
+            cross = copy.deepcopy(dataset[idx]['cross'])
+            looking = copy.deepcopy(dataset[idx]['looking'])
+            gesture = copy.deepcopy(dataset[idx]['gesture'])
+            next_transition = copy.deepcopy(dataset[idx]["next_transition"])
+            attributes = copy.deepcopy(dataset[idx]["attributes"])
+
+            for i in range(len(frames)):
+                key = None
+                old_id = None
+                d1 = min(i, 5)
+                d2 = min(len(frames) - i - 1, 5)
+                if mode == "GO":
+                    if next_transition[i] == 0 and action[i] == 1 and action[i - d1] == 0 and action[i + d2] == 1:
+                        j += 1
+                        new_id = "{:04d}".format(j) + "_" + self.name
+                        key = "JG_" + new_id
+                        old_id = f'{idx}/{vid_id}/' + '{:03d}'.format(frames[i])
+                if mode == "STOP":
+                    if next_transition[i] == 0 and action[i] == 0 and action[i - d1] == 1 and action[i + d2] == 0:
+                        j += 1
+                        new_id = "{:04d}".format(j) + "_" + self.name
+                        key = "JS_" + new_id
+                        old_id = f'{idx}/{vid_id}/' + '{:03d}'.format(frames[i])
+
+                if key is not None:
+                    samples[key] = {}
+                    samples[key]["source"] = "JAAD"
+                    samples[key]["old_id"] = old_id
+                    samples[key]['video_number'] = vid_id
+                    samples[key]['frame'] = frames[i]
+                    samples[key]['bbox'] = bbox[i]
+                    samples[key]['action'] = action[i]
+                    samples[key]['cross'] = cross[i]
+                    samples[key]['looking'] = looking[i]
+                    samples[key]['gesture'] = gesture[i]
+                    samples[key]["attributes"] = attributes
+        if verbose:
+            print(f"Extract {len(samples.keys())} {mode} sample frames from JAAD {self.name} set")
+
+        return samples
---
+++ pie_trans.py
@@ -0,0 +1,202 @@
+import pickle
+
+import numpy as np
+import copy
+
+
+def get_ped_ids_pie(annotations, image_set="all"):
+    """
+    Returns all pedestrian ids
+    :return: A list of pedestrian ids
+    """
+    pids = []
+    image_set_nums = {'train': ['set01', 'set02', 'set04'],
+                      'val': ['set05', 'set06'],
+                      'test': ['set03'],
+                      'all': ['set01', 'set02', 'set03',
+                              'set04', 'set05', 'set06']}
+    set_ids = image_set_nums[image_set]
+    for sid in set_ids:
+        for vid in sorted(annotations[sid]):
+            pids.extend(annotations[sid][vid]['ped_annotations'].keys())
+
+    return pids
+
+
+def get_ped_info_pie(annotations, image_set="all"):
+    # get ped information,i.e. frames,bbox,occlusion, actions(walking or not)
+    pids = get_ped_ids_pie(annotations, image_set)
+    dataset = annotations
+    ped_info = {}
+    for idx in pids:
+        sn, vn, _ = idx.split("_")
+        sid = "set{:02d}".format(int(sn))
+        vid = "video_{:04d}".format(int(vn))
+        ped_info[idx] = {}
+        ped_info[idx]["set_number"] = sid
+        ped_info[idx]["video_number"] = vid
+        ped_info[idx]['frames'] = copy.deepcopy(dataset[sid][vid]['ped_annotations'][idx]['frames'])
+        ped_info[idx]['bbox'] = copy.deepcopy(dataset[sid][vid]['ped_annotations'][idx]['bbox'])
+        ped_info[idx]['occlusion'] = copy.deepcopy(dataset[sid][vid]['ped_annotations'][idx]['occlusion'])
+        ped_info[idx]['action'] = copy.deepcopy(dataset[sid][vid]['ped_annotations'][idx]['behavior']['action'])
+        ped_info[idx]['looking'] = copy.deepcopy(dataset[sid][vid]['ped_annotations'][idx]['behavior']['look'])
+        ped_info[idx]['gesture'] = copy.deepcopy(dataset[sid][vid]['ped_annotations'][idx]['behavior']['gesture'])
+        ped_info[idx]['cross'] = copy.deepcopy(dataset[sid][vid]['ped_annotations'][idx]['behavior']['cross'])
+        ped_info[idx]['attributes'] = copy.deepcopy(dataset[sid][vid]['ped_annotations'][idx]['attributes'])
+
+    return ped_info
+
+
+def filter_None(x):
+    # small help function to filter None in list
+    if x is None:
+        return False
+    else:
+        return True
+
+
+def ped_info_clean_pie(annotations, image_set="all"):
+    """
+     Remove all fully occluded frames
+    """
+    ped_info = get_ped_info_pie(annotations, image_set)
+    ids = list(ped_info.keys())
+    # remove all frames with occlusion tag=2
+    for idx in ids:
+        occ = np.array(ped_info[idx]['occlusion'])
+        full_occ = np.flatnonzero(occ == 2)
+        # set fully occluded frames to None
+        for i in range(len(full_occ)):
+            ped_info[idx]['frames'][full_occ[i]] = None
+            ped_info[idx]['bbox'][full_occ[i]] = None
+            ped_info[idx]['action'][full_occ[i]] = None
+            ped_info[idx]['occlusion'][full_occ[i]] = None
+            ped_info[idx]['looking'][full_occ[i]] = None
+            ped_info[idx]['cross'][full_occ[i]] = None
+            ped_info[idx]['gesture'][full_occ[i]] = None
+
+        # filter all None values
+        ped_info[idx]['frames'] = list(filter(filter_None, ped_info[idx]['frames']))
+        ped_info[idx]['bbox'] = list(filter(filter_None, ped_info[idx]['bbox']))
+        ped_info[idx]['action'] = list(filter(filter_None, ped_info[idx]['action']))
+        ped_info[idx]['occlusion'] = list(filter(filter_None, ped_info[idx]['occlusion']))
+        ped_info[idx]['looking'] = list(filter(filter_None, ped_info[idx]['looking']))
+        ped_info[idx]['cross'] = list(filter(filter_None, ped_info[idx]['cross']))
+        ped_info[idx]['gesture'] = list(filter(filter_None, ped_info[idx]['gesture']))
+
+    return ped_info
+
+
+def add_transition_labels_pie(dataset, verbose=False):
+    """
+    Add labels to show the time (number of frames)
+    away from next action transition
+    """
+    all_wts = 0  # walking to standing
+    all_stw = 0  # standing to walking
+    ped_ids = list(dataset.keys())
+    for idx in ped_ids:
+        action = dataset[idx]['action']
+        frames = dataset[idx]['frames']
+        n_frames = len(frames)
+        dataset[idx]['next_transition'] = []
+        stw_time = []
+        wts_time = []
+
+        for j in range(len(action) - 1):
+            if action[j] == 0 and action[j + 1] == 1:
+                all_stw += 1
+                stw_time.append(frames[j + 1])
+            elif action[j] == 1 and action[j + 1] == 0:
+                all_wts += 1
+                wts_time.append(frames[j + 1])
+
+        # merge
+        trans_time_ped = np.array(sorted(stw_time + wts_time))
+        # set transition tag
+        for i in range(n_frames):
+            t = frames[i]
+            future_trans_ped = trans_time_ped[trans_time_ped >= t]
+            if future_trans_ped.size > 0:
+                next_trans_ped = future_trans_ped[0]
+                dataset[idx]['next_transition'].append(next_trans_ped - t)
+            else:
+                dataset[idx]['next_transition'].append(None)
+
+    if verbose:
+        print('\n')
+        print('----------------------------------------------------------------')
+        print(f'Total number of standing to walking transitions (raw): {all_stw}')
+        print(f'Total number of walking to standing transitions  (raw): {all_wts}')
+
+    return None
+
+
+def build_ped_dataset_pie(pie_anns_path, image_set="all", verbose=False):
+    pie_anns = pickle.load(open(pie_anns_path, 'rb'))
+    ped_dataset = ped_info_clean_pie(pie_anns, image_set)
+    add_transition_labels_pie(ped_dataset, verbose)
+
+    return ped_dataset
+
+
+# -----------------------------------------------
+class pie_trans_dataset():
+    # dataset class for pedestrian samples
+    def __init__(self, pie_anns_path, image_set="all", verbose=False):
+        assert image_set in ['train', 'test', 'val', "all"]
+        self.dataset = build_ped_dataset_pie(pie_anns_path, image_set=image_set, verbose=verbose)
+        self.name = image_set
+
+    def extract_trans_frame(self, mode="GO", verbose=False):
+        dataset = self.dataset
+        assert mode in ["GO", "STOP"], "Transition type should be STOP or GO"
+        ids = list(dataset.keys())
+        samples = {}
+        j = 0
+        for idx in ids:
+            sid = copy.deepcopy(dataset[idx]['set_number'])
+            vid = copy.deepcopy(dataset[idx]['video_number'])
+            frames = copy.deepcopy(dataset[idx]['frames'])
+            bbox = copy.deepcopy(dataset[idx]['bbox'])
+            action = copy.deepcopy(dataset[idx]['action'])
+            cross = copy.deepcopy(dataset[idx]['cross'])
+            looking = copy.deepcopy(dataset[idx]["looking"])
+            gesture = copy.deepcopy(dataset[idx]["gesture"])
+            next_transition = copy.deepcopy(dataset[idx]["next_transition"])
+            attributes = copy.deepcopy(dataset[idx]["attributes"])
+
+            for i in range(len(frames)):
+                key = None
+                old_id = None
+                d1 = min(i, 5)
+                d2 = min(len(frames) - i - 1, 5)
+                if mode == "GO":
+                    if next_transition[i] == 0 and action[i] == 1 and action[i - d1] == 0 and action[i + d2] == 1:
+                        j += 1
+                        new_id = "{:04d}".format(j) + "_" + self.name
+                        key = "PG_" + new_id
+                        old_id = f'{idx}/' + '{:03d}'.format(frames[i])
+                if mode == "STOP":
+                    if next_transition[i] == 0 and action[i] == 0 and action[i - d1] == 1 and action[i + d2] == 0:
+                        j += 1
+                        new_id = "{:04d}".format(j) + "_" + self.name
+                        key = "PS_" + new_id
+                        old_id = f'{idx}/' + '{:03d}'.format(frames[i])
+                if key is not None:
+                    samples[key] = {}
+                    samples[key]["source"] = "PIE"
+                    samples[key]['set_number'] = sid
+                    samples[key]['video_number'] = vid
+                    samples[key]["old_id"] = old_id
+                    samples[key]['frame'] = frames[i]
+                    samples[key]['bbox'] = bbox[i]
+                    samples[key]['action'] = action[i]
+                    samples[key]['cross'] = cross[i]
+                    samples[key]['looking'] = looking[i]
+                    samples[key]['gesture'] = gesture[i]
+                    samples[key]["attributes"] = attributes
+        if verbose:
+            print(f"Extract {len(samples.keys())} {mode} sample frames from PIE {self.name} set")
+
+        return samples
