2b382d3e471153e96e226bea50f9234de2413960
DongxuGuo1997 <ericguohit@outlook.com>
2021-05-03 15:20:55 +0200

    update baselines

--- src/model/basenet.py
+++ src/model/basenet.py
@@ -1,10 +1,11 @@
 import logging
 import torch
+import torchvision.models
 
 LOG = logging.getLogger(__name__)
 
 
-# --------------------------------------------
+#--------------------------------------------
 # helpers
 
 def use_pifpaf_bn(module):
@@ -17,12 +18,17 @@ def use_pifpaf_bn(module):
 def freeze_bn(module):
     if isinstance(module, torch.nn.modules.batchnorm._BatchNorm):
         module.eval()
+        
 
-
+def set_conv2d_stride1(module):
+    if isinstance(module, torch.nn.modules.conv.Conv2d):
+        module.stride = (1,1)  
+        
+        
 class ResnetBlocks():
     def __init__(self, resnet):
         self.modules = list(resnet.children())
-        # LOG.debug('modules = %s', self.modules)
+        #LOG.debug('modules = %s', self.modules)
 
     def input_block(self, use_pool=False, conv_stride=2, pool_stride=2):
         modules = self.modules[:4]
@@ -49,33 +55,59 @@ class ResnetBlocks():
 
     def block5(self):
         return self.modules[7]
-
-    def build_backbone(self, use_pool=False, use_last_block=True, pifpaf_bn=True):
+    
+    def build_backbone(self, use_pool=False,use_last_block = True, pifpaf_bn = True):
         input_modules = self.modules[:4]
         if not use_pool:
             input_modules.pop(3)
-
+            
         input_block = torch.nn.Sequential(*input_modules)
         block2 = self.modules[4]
         block3 = self.modules[5]
         block4 = self.modules[6]
         block5 = self.modules[7]
-
+        
         if use_last_block:
             backbone_net = torch.nn.Sequential(
-                input_block,
-                block2,
-                block3,
-                block4,
-                block5)
+                           input_block,
+                           block2,
+                           block3,
+                           block4,
+                           block5)
         else:
             backbone_net = torch.nn.Sequential(
-                input_block,
-                block2,
-                block3,
-                block4)
+                           input_block,
+                           block2,
+                           block3,
+                           block4)
         if pifpaf_bn:
             backbone_net.apply(use_pifpaf_bn)
 
+        
+        
         # return backbone
         return backbone_net
+
+
+class BaselineRand(torch.nn.Module):
+    def __init__(self):
+        super(BaselineRand, self).__init__()
+        # self.net = torch.nn.Identity()
+
+    def forward(self, x):
+        # x = self.net(x)
+        x = torch.rand(1)
+
+        return x
+    
+    
+class BaselineOne(torch.nn.Module):
+    def __init__(self):
+        super(BaselineOne, self).__init__()
+        # self.net = torch.nn.Identity()
+
+    def forward(self, x):
+        # x = self.net(x)
+        x = torch.tensor(1.0)
+
+        return torch.unsqueeze(x,0)
